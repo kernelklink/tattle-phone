c48657d9e76ac87318d285fdb606c1ba51dfa612
Will Fletcher <will.fletcher.ny@gmail.com>
2022-06-04 12:18:59 -0400

    Initial Commit
    Tattle core starts up and instantiates a hook-monitor thread. Currently
    just prints out the hook status when it's off. Next step: dial tone

---
+++ src/hook_monitor.py
@@ -0,0 +1,121 @@
+# hook_monitor
+# 
+# Monitors the hook
+
+import RPi.GPIO as GPIO
+from queue import Queue
+from threading import Thread, Lock
+import logging
+import time
+from enum import IntEnum
+
+class HookState(IntEnum):
+    HOOK_ON=0
+    HOOK_OFF=1
+
+class HookMonitor(Thread):
+    """Monitor the hook switch and signal when the phone is off/on the hook
+    """
+
+    def __init__(self, hook_pin, input_queue, output_queue, timeout=5) -> None:
+        """Constructor for HookMonitor object, assumes that the caller has already setup the GPIO pin, but we still need to setup the interrupts.
+
+        Args:
+            hook_pin (int): the GPIO pin number to monitor for the hook switch
+            input_queue (queue): input queue to receive communication from the owner
+            output_queue (queue): output queue to signal hook changes to the owner
+        """
+        super().__init__()
+
+        # Save some state
+        self._hook_pin = hook_pin
+        self._input_queue = input_queue
+        self._output_queue = output_queue
+        self._hook_state = HookState.HOOK_ON
+        self._lock = Lock()
+        self._timeout = timeout
+        self.name="HookMonitor"
+    
+    def hook_state(self) -> HookState:
+        """Return the current hook state.
+
+        Returns:
+            bool: True if we're 
+        """
+        with self._lock:
+            return self._hook_state
+    
+    @staticmethod
+    def hook_state_to_str(state:HookState)->str:
+        """Convert the given state to a string
+
+        Args:
+            state (HookState): The state of the hoook we're interested in
+
+        Returns:
+            str: Name of the state, e.g. HOOK_OFF
+        """
+        if( state == HookState.HOOK_OFF ):
+            return "HOOK_OFF"
+        elif( state == HookState.HOOK_ON ):
+            return "HOOK_ON"
+    
+    def hook_change(self, pin):
+        """Monitor the hook for changes, when it changes report that through the output queue
+
+        Args:
+            pin (int): GPIO pin associated with this change in current
+        """
+        if( self.running ):
+            with self._lock:
+                self._hook_state = HookState(GPIO.input(pin))
+            self._output_queue.put( self._hook_state )
+            logging.debug("Something changed on the hook, current value is {}".format(self._hook_state))
+        else:
+            logging.debug("Looks like I'm not running, but I'm getting interrupts")
+
+    
+    def run(self):
+        """Setup the interrupt for the hook GPIO pin and monitor it, communicating state back to the caller
+        """
+        self.running = True
+        with self._lock:
+            self._hook_state = HookState(GPIO.input(self._hook_pin))
+        GPIO.add_event_detect( self._hook_pin, GPIO.BOTH, self.hook_change )
+        
+        # Wait for someone to kill me
+        while self.running:
+            item = self._input_queue.get()
+            if( item == "KILL" ):
+                self.running = False
+            else:
+                logging.info("Received an unknown message from input_queue: '{}'".format(item))
+            
+            # sleep for a while
+            time.sleep(self._timeout)
+            
+        logging.info('Exiting...')
+
+if __name__ == "__main__":
+    # Setup GPIO
+    hook_pin = 12
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(hook_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    # Run a test to see if we can monitor the hook
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    hook_input_queue = Queue()
+    hook_output_queue = Queue()
+    hook_monitor = HookMonitor(hook_pin, hook_input_queue, hook_output_queue)
+    hook_monitor.start()
+
+    hook_changes = 0
+    while( hook_changes < 5 ):
+        change = hook_output_queue.get(30)
+        hook_changes += 1
+        logging.info( "Hook change: {}".format(change))
+    hook_input_queue.put("KILL")
+    hook_monitor.join()
+    GPIO.cleanup()
---
+++ src/tattle_core.py
@@ -0,0 +1,63 @@
+# tattle_core.py
+#
+# Sits in the middle and keeps everything running
+
+import RPi.GPIO as GPIO
+from hook_monitor import HookMonitor, HookState
+from dial_monitor import DialMonitor
+import argparse
+from queue import Queue
+from threading import Event
+import logging
+import time
+
+_HOOK_TIMEOUT_SEC = 0.1
+_JOIN_TIMEOUT_SEC = 10
+
+# Values that we'll use for 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--hook_pin", help="GPIO pin where the hook circuit is connected", type=int, default=12)
+    args = parser.parse_args()
+
+    # Setup GPIO
+    hook_pin = 12
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(args.hook_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    # Setup logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    # Instantiate the hook monitor
+    hook_input_queue = Queue()
+    hook_output_queue = Queue()
+    hook_monitor = HookMonitor(hook_pin, hook_input_queue, hook_output_queue)
+    hook_monitor.start()
+    time.sleep(1)
+
+    hook_state = hook_monitor.hook_state()
+    logging.debug("Initial hookstate = {}".format(hook_state))
+    # Simple test to monitor hook changes
+    while( 1 ):
+
+        if( hook_state == HookState.HOOK_OFF ):
+            logging.debug( "Off the hook!" )
+
+        # See if there's been a change to the hook
+        if( not hook_output_queue.empty()):
+            hook_change = hook_output_queue.get()
+            if( hook_change != hook_state ):
+                logging.debug( "Received a hook change, new value is {}".format(HookMonitor.hook_state_to_str(hook_change)))
+                hook_state = hook_change
+        
+        # Short sleep for this cycle
+        time.sleep(0.1)
+
+    # Cleanup
+    hook_input_queue.put("KILL")
+    hook_monitor.join(_JOIN_TIMEOUT_SEC)
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    main()
