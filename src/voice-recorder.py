50c19bdb5729e223bedc6ea409988c9ad3c2c095
Will Fletcher <13805043+kernelklink@users.noreply.github.com.>
2024-12-08 17:19:15 -0500

    Refined VoiceRecorder
    Figured out what I need and what I don't need, plus, how I'm going to do IPC.  Basically sticking with an Event to coordinate killing the process off.

--- src/voice-recorder.py
+++ src/voice-recorder.py
@@ -3,38 +3,48 @@
 # A class that will cheat and use arecord to record voice messages to a file
 
 import subprocess
-from queue import Queue
-from threading import Lock, Timer, Event
+from threading import Event
 import shutil
 import logging
 import time
+from datetime import datetime
 
 class VoiceRecorder():
-    def __init__(self, filename: str, input_queue: Queue):
-        self.input_queue = input_queue
-        self.filename = filename
+    """VoiceRecorder class is very direct, basically just records to a file.
+    """
+    def __init__(self, filename: str):
         self.name = "VoiceRecorder"
-        self.executable = 'arecord'
+        self._filename = filename
+        self._executable = 'arecord'
+        self._kill_event = Event()
     
     def run(self):
-        
+        """Uses arecorord to record to the given wavefile
+        """
         # Figure out the location of the executable
-        self.executable = shutil.which(self.executable)
-        args = [self.executable, self.filename]
+        self._executable = shutil.which(self._executable)
+        args = [self._executable, self._filename]
 
         # Start recording to file
-        proc = subprocess.Popen(
-            args
-        )
-        time.sleep(8)
-        
-        proc.kill()
-            
-            
+        proc = subprocess.Popen(args)
 
+        # Wait until someone tells us to die
+        self._kill_event.wait(timeout=120)        
+
+        # Die
+        proc.kill()
+    
+    def kill(self):
+        """Kill the subprocess we started
+        """
+        self._kill_event.set()
 
 if (__name__ == "__main__"):
-    recorder_queue = Queue()
-    recorder = VoiceRecorder("hudson_test.wav", recorder_queue)
-
+    """Run a quick test of voice recorder by recording to the given file.
+    """
+    print("This test will record a wave file for 8 seconds and then die.")
+    recorder = VoiceRecorder("hudson_test.wav")
+    
     recorder.run()
+    time.sleep(8)
+    recorder.kill()
