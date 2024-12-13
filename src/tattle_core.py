fcee2fa84ff639fa1079014c9f0132c1fa54ad94
Will Fletcher <13805043+kernelklink@users.noreply.github.com>
2024-12-12 21:51:57 -0500

    Reworked TattleCore into a class
    It just feels cleaner to me to have member variables instead of big
    globals. So made it a class, made playback its own function. I'll
    probably do the same for voice recording, I like the idea of the
    functions performing and just returning the next state we should be in.
    Although, even that is too much, probably.

--- src/tattle_core.py
+++ src/tattle_core.py
@@ -22,7 +22,21 @@ import re
 from datetime import datetime
 from pathlib import Path
 
-_RECORDING_DIR = "../tattles"
+_MONTH_MAP = {
+    1:  "January",
+    2:  "February",
+    3:  "March",
+    4:  "April",
+    5:  "May",
+    6:  "June",
+    7:  "July",
+    8:  "August",
+    9:  "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+_RECORDING_DIR = "/var/lib/tattles"
 _RECORDING_RE = re.compile( r"(?P<year>\d\d\d\d)-(?P<month>\d\d)-(?P<day>\d\d)_(?P<hour>\d\d)(?P<minute>\d\d)(?P<second>\d\d)\.wav")
 
 _PLAYBACK_TEXT = "Tattled on {month} {day} at {hour} {minute} {am_pm}"
@@ -46,54 +60,206 @@ _JOIN_TIMEOUT_SEC = 10
 
 _SPEECH_UTIL = shutil.which('espeak-ng')
 
-def play_text(text_to_speak:str):
-    """Plays the specified text from the speaker
+class TattlePhone():
 
-    Args:
-        text_to_speak (str): String to convert to speech
-    """
-    args = [_SPEECH_UTIL, 
-            "-ven-us+f2",
-            text_to_speak]
-    subprocess.run(args)
+    def change_state(self, new_state:TattleState) -> TattleState:
+        logging.debug(f"Changing from {self._state.name} to {new_state.name}")
+        self._state = new_state
 
-def play_recordings():
-    files = list(scandir(_RECORDING_DIR))
-    files = sorted( files, key=lambda x: x.stat().st_mtime, reverse=True)
-
-    for f in files:
-        match = _RECORDING_RE.match(f.name)
-        if( not match ):
-            continue
-
+    @staticmethod
+    def get_intro_text(file_name:str) -> str:
+        match = _RECORDING_RE.match(file_name)
+        if( match is None ):
+            return None
+        
+        # Figure out AM / PM
         hour = match.group('hour')
         am_pm = "A.M."
         if( int(hour) > 12 ):
             hour = str(int(hour) - 12)
             am_pm = "P.M."
-
+        
+        # Figure out minute
+        minute = int(match.group('minute'))
+        minute_str = match.group('minute')
+        if(minute == 0):
+            minute_str = "o'clock"
+        elif(minute < 10 ):
+            minute_str = f"O {minute}"
+        
         # Construct intro
-        text = _PLAYBACK_TEXT.format(
-            month=match.group('month'),
-            day=match.group('day'),
+        intro_text = _PLAYBACK_TEXT.format(
+            month=_MONTH_MAP[int(match.group('month'))],
+            day=int(match.group('day')),
             hour=hour,
-            minute=match.group('minute'),
+            minute=minute_str,
             am_pm=am_pm
         )
-        # Play intro and message
-        play_text(text)
-        time.sleep(0.1)
+        return intro_text
+    
+    @staticmethod
+    def get_filename() -> str:
+        """Get a filename with the current time embedded
+
+        Returns:
+            str: Filanme of the format YYYY-MM-DD_HHMMSS.wav
+        """
+        dt = datetime.now()
+        return dt.strftime("%Y-%m-%d_%H%M%S") + ".wav"
+
+    def __init__(self):
+        self._my_input_queue = Queue()
+        self._state = TattleState.TATTLE_IDLE
         
-def get_filename() -> str:
-    """Get a filename with the current time embedded
+        # Instantiate the audio player
+        self.audio_player = AudioPlayer(self._my_input_queue)
+        self.audio_player.start()
 
-    Returns:
-        str: Filanme of the format YYYY-MM-DD_HHMMSS.wav
-    """
-    dt = datetime.now()
-    return dt.strftime("%Y-%m-%d_%H%M%S") + ".wav"
+        # Instantiate the hook monitor
+        self.hook_monitor = HookMonitor(args.hook_pin, self._my_input_queue)
+        self.hook_input_queue = self.hook_monitor.input_queue()
+        self.hook_monitor.start()
+        
+        # Instantiate the dial monitor
+        self.dial_monitor = DialMonitor(args.dial_pin, self._my_input_queue)
+        self.dial_input_queue = self.dial_monitor.input_queue()
+        self.dial_monitor.start()
 
-def main():
+        # Give our threads a moment to start
+        time.sleep(1)
+
+    def run(self):
+        # Make a reference to the recorder
+        self.voice_recorder = None
+
+        # Start the state machine
+        self._state = TattleState.TATTLE_IDLE
+        self.hook_state = self.hook_monitor.hook_state()
+        logging.debug("Initial hookstate = {}".format(self.hook_state))
+        if( self.hook_state == HookState.HOOK_OFF):
+            self.change_state(TattleState.TATTLE_MENU_ROOT)
+
+        while( 1 ):
+            logging.debug(f"Currently in {self._state.name}")
+
+            if( self._state == TattleState.TATTLE_IDLE ):
+                # Wait for a hook change
+                source,item = self._my_input_queue.get()
+                if( source == "HOOK" and HookState(item) != self.hook_state ):
+                    self.hook_state = item
+                    self.change_state(TattleState.TATTLE_MENU_ROOT)
+                else:
+                    logging.debug(f"Received Unhandled Event: {source}:{item}")
+                    
+            elif( self._state == TattleState.TATTLE_MENU_ROOT ):
+                # Playback menu selection
+                self.audio_player.play_text(
+                        "To tattle on someone, please dial {record}. To listen to the tattling of others, please dial {playback}" \
+                        .format(record=TattleRootMenu.ROOT_MENU_RECORD.value, 
+                                playback=TattleRootMenu.ROOT_MENU_PLAYBACK.value) )
+                
+                # Wait for audio to finish
+                source,item = self._my_input_queue.get()
+                if( source == "AUDIO" ):
+                    logging.debug("No selection was made, coming back around.")
+                
+                # Phone has been hung up
+                elif( source == "HOOK" and HookState(item) != self.hook_state ):
+                    logging.debug("Hung up while playing menu, return to idle")
+                    self.audio_player.stop()
+
+                    # Change state
+                    self.hook_state = item
+                    self.change_state(TattleState.TATTLE_IDLE)
+                
+                # Someone made a selection
+                elif( source == "DIAL" ):
+                    logging.debug("A number has been dialed.")
+                    self.audio_player.stop()
+
+                    # Selected record
+                    if( item == TattleRootMenu.ROOT_MENU_RECORD.value ):
+                        self.change_state(TattleState.TATTLE_RECORD)
+                    
+                    # Selected playback
+                    elif( item == TattleRootMenu.ROOT_MENU_PLAYBACK.value ):
+                        self.change_state(TattleState.TATTLE_PLAYBACK)
+                    
+                    else:
+                        logging.debug(f"Someone dialed {item}, not valid.")
+                else:
+                    logging.debug(f"Received Unhandled Event: {source}:{item}")
+                
+            elif( self._state == TattleState.TATTLE_RECORD ):
+                # Create a voice recording
+                if( self.voice_recorder is None ):
+                    # Play the beep
+                    self.audio_player.play_file(_BEEP_WAV)
+
+                    filename = Path(_RECORDING_DIR,self.get_filename())
+                    logging.debug(f"Creating recording {filename}")
+                    self.voice_recorder = VoiceRecorder(filename)
+                    self.voice_recorder.start()
+
+                # Wait for a hook change
+                while(self.hook_state != HookState.HOOK_ON ):
+                    source,item = self._my_input_queue.get()
+                    if( source == "HOOK" and HookState(item) != self.hook_state ):
+                        self.hook_state = item
+                        self.change_state(TattleState.TATTLE_IDLE)
+                    else:
+                        logging.debug(f"Received Unhandled Event: {source}:{item}")
+                
+                # Kill our recording
+                self.voice_recorder.kill()
+                self.voice_recorder.join()
+                self.voice_recorder = None
+
+            elif( self._state == TattleState.TATTLE_PLAYBACK ):
+                destination_state = self.playback()
+                self.change_state(destination_state)
+
+        # Cleanup
+        self.hook_input_queue.put("KILL")
+        self.hook_monitor.join(_JOIN_TIMEOUT_SEC)
+        self.dial_input_queue.put("KILL")
+        self.dial_monitor.join(_JOIN_TIMEOUT_SEC)
+        self.audio_player.kill()
+        self.audio_player.join()
+        GPIO.cleanup()
+    
+    def playback(self) -> TattleState:
+        files = list(scandir(_RECORDING_DIR))
+        files = sorted( files, key=lambda x: x.name, reverse=True)
+        for file in files:
+            intro_text = self.get_intro_text(file.name)
+            if( intro_text is None ):
+                continue
+            
+            logging.debug("Queuing up text and file to play")
+            self.audio_player.play_text(intro_text)
+            self.audio_player.play_file(file)
+            still_playing = True
+            while( still_playing ):
+                source,item = self._my_input_queue.get()
+                if( source == "HOOK" and HookState(item) != self.hook_state ):
+                    logging.debug("Phone was hung up, stopping audio")
+                    self.audio_player.stop()
+                    self.hook_state = item
+                    return TattleState.TATTLE_IDLE
+                elif( source == "AUDIO" ):
+                    logging.debug("Looks like the audio player is free, let's move on!")
+                    still_playing = False
+                elif( source == "DIAL" and item == 1 ):
+                    logging.debug("Skipping!")
+                    self.audio_player.stop()
+                    still_playing = False
+                else:
+                    logging.debug(f"Received Unhandled Event: {source}:{item}")
+        
+        return TattleState.TATTLE_MENU_ROOT
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--hook_pin", help="GPIO pin where the hook circuit is connected", type=int, default=12)
     parser.add_argument("--dial_pin", help="GPIO pin where the dial circuit is connected", type=int, default=16)
@@ -107,126 +273,6 @@ def main():
     # Setup logging
     logging.basicConfig(level=logging.DEBUG)
 
-    # Setup inputs
-    my_input_queue = Queue()
-
-    # Instantiate the hook monitor
-    hook_monitor = HookMonitor(args.hook_pin, my_input_queue)
-    hook_input_queue = hook_monitor.input_queue()
-    hook_monitor.start()
-    
-    # Instantiate the dial monitor
-    dial_monitor = DialMonitor(args.dial_pin, my_input_queue)
-    dial_input_queue = dial_monitor.input_queue()
-    dial_monitor.start()
-
-    # Instantiate the audio player
-    audio_player = AudioPlayer(my_input_queue)
-    audio_player.start()
-
-    # Give our threads a moment to start
-    time.sleep(1)
-
-    # Make a reference to the recorder
-    voice_recorder = None
-
-    # Start the state machine
-    state = TattleState.TATTLE_IDLE
-
-    hook_state = hook_monitor.hook_state()
-    logging.debug("Initial hookstate = {}".format(hook_state))
-    if( hook_state == HookState.HOOK_OFF):
-        state = TattleState.TATTLE_MENU_ROOT
-
-    while( 1 ):
-        logging.debug(f"Currently in {state.name}")
-
-        if( state == TattleState.TATTLE_IDLE ):
-            # Wait for a hook change
-            source,item = my_input_queue.get()
-            if( source == "HOOK" and item != hook_state ):
-                hook_state = item
-                logging.debug(f"Changing from {state.name} to {TattleState.TATTLE_MENU_ROOT.name}")
-                state = TattleState.TATTLE_MENU_ROOT
-                
-        elif( state == TattleState.TATTLE_MENU_ROOT ):
-            # Playback menu selection
-            audio_player.play_text(
-                    "To tattle on someone, please dial {record}. To listen to the tattling of others, please dial {playback}" \
-                    .format(record=TattleRootMenu.ROOT_MENU_RECORD.value, 
-                            playback=TattleRootMenu.ROOT_MENU_PLAYBACK.value) )
-            
-            # Wait for a number input or hook change
-            source,item = my_input_queue.get()
-            if( source == "AUDIO" and item == "DONE" ):
-                logging.debug("No selection was made, coming back around.")
-            else:
-                # No matter the input, we want to kill the audio
-                audio_player.stop()
-
-            # Phone has been hung up
-            if( source == "HOOK" and item != hook_state ):
-                # Change state
-                hook_state = item
-                logging.debug(f"Changing from {state.name} to {TattleState.TATTLE_IDLE.name}")
-                state = TattleState.TATTLE_IDLE
-            
-            # Someone made a selection
-            elif( source == "DIAL" ):
-                if( item == TattleRootMenu.ROOT_MENU_RECORD.value ):
-                    logging.debug(f"Changing from {state.name} to {TattleState.TATTLE_RECORD.name}")
-                    state = TattleState.TATTLE_RECORD
-                elif( item == TattleRootMenu.ROOT_MENU_PLAYBACK.value ):
-                    logging.debug(f"Changing from {state.name} to {TattleState.TATTLE_PLAYBACK.name}")
-                    state = TattleState.TATTLE_PLAYBACK
-            
-        elif( state == TattleState.TATTLE_RECORD ):
-            # Create a voice recording
-            if( voice_recorder is None ):
-                # Play the beep
-                audio_player.play_file(_BEEP_WAV)
-
-                filename = Path(_RECORDING_DIR,get_filename())
-                logging.debug(f"Creating recording {filename}")
-                voice_recorder = VoiceRecorder(filename)
-                voice_recorder.start()
-
-            # Wait for a hook change
-            while(hook_state != HookState.HOOK_ON ):
-                source,item = my_input_queue.get()
-                if( source == "HOOK" and item != hook_state ):
-                    hook_state = item
-                    logging.debug(f"Changing from {state.name} to {TattleState.TATTLE_IDLE.name}")
-                    state = TattleState.TATTLE_IDLE
-            
-            # Kill our recording
-            voice_recorder.kill()
-            voice_recorder.join()
-            voice_recorder = None
-
-        elif( state == TattleState.TATTLE_PLAYBACK ):
-            # Start playback thread
-            audio_player.play_text(
-                "I'm sorry, playback has not yet been enabled on this device. Please tattle on me.")
-            
-
-            logging.debug(f"Changing from {state.name} to {TattleState.TATTLE_MENU_ROOT.name}")
-            state = TattleState.TATTLE_MENU_ROOT
-            # # Wait for a hook change
-            # source,item = my_input_queue.get()
-            # if( source == "HOOK" and item != hook_state ):
-            #     hook_state = item
-            #     logging.debug(f"Changing from {state.name} to {TattleState.TATTLE_IDLE.name}")
-            #     state = TattleState.TATTLE_IDLE
-
-    # Cleanup
-    hook_input_queue.put("KILL")
-    hook_monitor.join(_JOIN_TIMEOUT_SEC)
-    dial_input_queue.put("KILL")
-    dial_monitor.join(_JOIN_TIMEOUT_SEC)
-    audio_player.kill()
-    audio_player.join()
-    GPIO.cleanup()
-
-if __name__ == "__main__":
-    main()
+    # Start the phone
+    tattle_phone = TattlePhone()
+    tattle_phone.run()
